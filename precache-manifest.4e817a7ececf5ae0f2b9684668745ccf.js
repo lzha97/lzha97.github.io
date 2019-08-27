4c7e1eb60c655066bc03b0e56dc30ffe472f3adf
Lillian Zha <lillianzha@gmail.com>
2019-08-26 19:54:40 -0500

    Updates

---
+++ precache-manifest.4e817a7ececf5ae0f2b9684668745ccf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c1757d2a9919142919fc215e33d504",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "0261292122796ed4009f",
+    "url": "/myapp/static/css/main.676ed426.chunk.css"
+  },
+  {
+    "revision": "59872f670841080a1b55",
+    "url": "/myapp/static/js/2.535a482d.chunk.js"
+  },
+  {
+    "revision": "0261292122796ed4009f",
+    "url": "/myapp/static/js/main.138ffe00.chunk.js"
+  },
+  {
+    "revision": "c6a82cf25f3a10c0ae1d",
+    "url": "/myapp/static/js/runtime~main.a5ed1510.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
